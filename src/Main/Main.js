5b539149b7a3682cd646e0d862ad571f3551647b
Simon-Oliver <stauffer.simon@outlook.com>
2019-06-25 21:06:24 +1000

    added grid to main content

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,6 +4,11 @@ import './Main.css';
 const Main = () => (
   <div className="mainContainer">
     <h1>Main Container</h1>
+    <div className="gridContainer">
+      <div className="gridOne">One</div>
+      <div className="gridTwo">Two</div>
+      <div className="gridThree">Three</div>
+    </div>
   </div>
 );
 
